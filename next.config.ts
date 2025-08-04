2de41a938339012c9b76d4e5ae0d8011ba8e1522
Takaki Inokuchi <inokutitakaki@yahoo.co.jp>
2025-08-04 12:23:52 +0900

    githubでの閲覧追加

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  basePath: "/cat-app",
+  assetPrefix: "/cat-app/",
 };
 
 export default nextConfig;
