37069688dbf23954ce47379825b1001248cd0545
Takaki Inokuchi <inokutitakaki@yahoo.co.jp>
2025-08-07 20:01:18 +0900

    スマホログイン

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,25 +1,42 @@
 import { createContext, useContext, useEffect, useState } from 'react';//createContextは親(pages)から子(firebaseAuth)に値を渡すため useContextで値を受け取る
-import { User, onAuthStateChanged } from 'firebase/auth';//Userはログインしているユーザーの情報,onAuthStateChangedはリアルタイムで状態を監視
+import { User, onAuthStateChanged, getRedirectResult, GoogleAuthProvider } from 'firebase/auth';//Userはログインしているユーザーの情報,onAuthStateChangedはリアルタイムで状態を監視
 import { auth } from '@/lib/firebaseAuth';//初期化済みオブジェクトを取得
 
 type AuthContextType = {
   user: User | null;//型を指定
+  loading: boolean;
 };//型エイリアスの定義、userというプロパティがあり、型は、userまたはnull
 
-const AuthContext = createContext<AuthContextType>({ user: null });//createContextを使用し、子にプロップスを渡せるように、渡す値はAuthContextTypeなので、userかnull←は初期値を指定して書く
+const AuthContext = createContext<AuthContextType>({
+  user: null,
+  loading: true,
+});//createContextを使用し、子にプロップスを渡せるように、渡す値はAuthContextTypeなので、userかnull←は初期値を指定して書く
 
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   const [user, setUser] = useState<User | null>(null);//React.ReactNodeは描画できるすべての型を指定、<User | null>でどちらかの型が入るということ、初期値はnullということ、ユニオン型今回は<user | null>二つの型の選択肢を与えている
+  const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-      setUser(currentUser);//ログイン状態が変化するたびにauth,(currentUser)←コールバックが呼び出される
-    });//引数はauthと関数のsetUserまで分かれていることに注意
+      setUser(currentUser);
+      setLoading(false);
+    });
+
+    getRedirectResult(auth)
+      .then((result) => {
+        if (result?.user) {
+          setUser(result.user);
+        }
+      })
+      .catch((error) => {
+        console.error('リダイレクトログインエラー:', error);
+      });
+
     return () => unsubscribe();
   }, []);//useEffectの中でreturnを使用すると監視をonAuthStateChangedを終了させるという意味
 
   return (
-    <AuthContext.Provider value={{ user }}>
+    <AuthContext.Provider value={{ user, loading }}>
       {children}
     </AuthContext.Provider>
   );//app.texで使用。囲まれた内容の中ではログイン状態が保存されるようになっている
