6dc9bd2b786241018e2753c9708cede99ed45b13
Takaki Inokuchi <inokutitakaki@yahoo.co.jp>
2025-08-12 20:52:06 +0900

    ファイル修正

--- src/lib/firebaseAuth.ts
+++ src/lib/firebaseAuth.ts
@@ -1,22 +1,11 @@
-import { initializeApp } from "firebase/app";//firebaseをアプリで使用するために初期化する関数
-import { getAuth, signInWithPopup, GoogleAuthProvider, signInWithRedirect, signOut } from "firebase/auth";//getAuthは認証機能を使用するための関数
+import { auth } from './firebase';
+import { signInWithPopup, GoogleAuthProvider, signInWithRedirect, signOut } from "firebase/auth";//getAuthは認証機能を使用するための関数
 
 const isMobile = (): boolean => {
-  if(typeof window === 'undefined') return false;//windowがなければ、falseが返される。→これがないとSSR中だと(next.js)では一部のコードはサーバー側で実行されることがあるため、何も返さない致命的なエラーになる。サーバーサイドレンダリング
+  if (typeof window === 'undefined') return false;//windowがなければ、falseが返される。→これがないとSSR中だと(next.js)では一部のコードはサーバー側で実行されることがあるため、何も返さない致命的なエラーになる。サーバーサイドレンダリング
   return /iPhone|iPod|Android/i.test(window.navigator.userAgent);//test(調べたい文字列)を使用してtrueかfalseの判定、window.navigatorでuserの情報を取得、スマホとかならture
 };
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAd1m63mLBAd9o1PVmlA88n3z1wc6P3JfM",
-  authDomain: "cat-app-9e6a8.firebaseapp.com",
-  projectId: "cat-app-9e6a8",
-  storageBucket: "cat-app-9e6a8.firebasestorage.app",
-  messagingSenderId: "374133148516",
-  appId: "1:374133148516:web:32d784a6d9490f7f3d0871"
-};
-
-const app = initializeApp(firebaseConfig);//firebaseConfig は Firebase コンソールで発行される設定情報で、APIキーやプロジェクトIDなどが入っています。認証機能を使用するにはどのプロジェクトを使用するか決定するため　initializeは初期化
-const auth = getAuth(app);//Authentication サービスのインスタンスを取得
 const provider = new GoogleAuthProvider();//GoogleAuthProvider→google用のプロバイダーを作成
 
 const loginWithGoogle = () => {
